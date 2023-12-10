68058ad9825cda154286f60a02b4c93d23504dd3
jmadupalli <jayanth.madupalli@gmail.com>
2023-12-09 18:00:47 -0600

    Initial Commit

---
+++ backend/shortly-api/src/main/java/com/jay/shortlyapi/ShortlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jay.shortlyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortlyApiApplication.class, args);
+	}
+
+}
---
+++ backend/shortly-api/src/test/java/com/jay/shortlyapi/ShortlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.shortlyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
